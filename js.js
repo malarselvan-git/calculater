20cd626649c2ad170cac5eb34ddc1d164ab97b5f
malarselvan-git <malarselvan5555@gmail.com>
2024-08-12 23:09:13 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+ function im1()
+ {
+    let a=prompt ('Enter the side of square');
+   
+    let res= a*a;
+   alert (`The area of the square: ${res}`)
+
+}
+function im2()
+{
+    let a=prompt('Subration')
+    let a1=prompt('Subration')
+   
+    let res= a-a1
+   
+    alert (`Answer: ${res}`)
+}
+
+function im3()
+{
+    let a=prompt('Multification')
+    let a1=prompt('Multification')
+    
+
+    let res= a*a1
+    alert (`Answer: ${res}`)
+}
+function im4()
+{
+    let a=prompt('Division')
+    let a1=prompt('Division')
+
+    let res= a/a1
+
+    alert (`Answer: ${res}`)
+}
+function im5()
+{
+    let a=prompt('Modulo Division')
+    let a1=prompt('Modulo Division')
+  
+     let res= a%a1
+
+     alert (`Answer: ${res}`)
+}
